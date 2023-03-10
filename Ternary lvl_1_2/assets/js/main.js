173215540278883cf6385bd47fcc270934c1d7af
Rizvan555 <rizvan84@gmx.de>
2023-03-10 09:29:59 +0100

    changed style

--- Ternary lvl_1_2/assets/js/main.js
+++ Ternary lvl_1_2/assets/js/main.js
@@ -1,11 +1,10 @@
-const input = document.querySelector("#input-container");
 const inputBox = document.querySelector(".input-box");
 const article = document.querySelector("#reply");
 
 function checker() {
   const password = inputBox.value;
-  const positive = "Welcome to your Account";
-  const negative = "Your password is short";
+  const positive = "<span style='color: green'>Welcome to your Account</span>";
+  const negative = "<span style='color: red'>Your password is short</span>";
 
   const reply = password.length >= 8 ? positive : negative;
 
